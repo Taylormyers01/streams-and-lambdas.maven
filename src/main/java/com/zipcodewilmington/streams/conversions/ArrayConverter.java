7cc1e1ceea154b9356c1c071da0300b1e5ad29e9
taylor <taylormyers01@comcast.net>
2023-08-10 10:08:27 -0400

    Finished up array conversions and verified all tests pass

--- src/main/java/com/zipcodewilmington/streams/conversions/ArrayConverter.java
+++ src/main/java/com/zipcodewilmington/streams/conversions/ArrayConverter.java
@@ -25,12 +25,12 @@ public final class ArrayConverter extends PersonConversionAgent<Person[]> {
 
     //TODO
     public List<Person> toList() {
-        return this.toStream().collect(Collectors.toList());
+        return Arrays.stream(super.objectSequence).collect(Collectors.toList());
     }
 
     //TODO
     public Stream<Person> toStream() {
-        return null;
+        return Arrays.stream(super.objectSequence);
     }
 
     @Override
